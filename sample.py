66fecfb19949e0eaf9a1754e39f6facf37463872
satya <38804803+satyamuralidhar@users.noreply.github.com>
2019-08-26 16:20:41 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+c = np.array([[1,2,3,4],[5,6,7,8],[9,8,7,6]],int)
+type(c)
+
+
+# In[2]:
+
+
+c
+
+
+# In[3]:
+
+
+c[1,1]
+
+
+# In[4]:
+
+
+c[2,3]
+
+
+# In[5]:
+
+
+c[2,0]
+
+
+# In[6]:
+
+
+k=c.reshape(4,3)
+
+
+# In[7]:
+
+
+k
+
+
+# In[8]:
+
+
+c.ndim
+
+
+# In[10]:
+
+
+c.shape
+
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+import numpy as np
+a=np.arange(5)
+
+
+# In[7]:
+
+
+a
+
+
+# In[8]:
+
+
+a=np.ones([3,3])
+
+
+# In[9]:
+
+
+a
+
+
+# In[10]:
+
+
+import numpy as np
+a=np.arange(5)
+
+
+# In[11]:
+
+
+a1=np.ones([3,2])
+
+
+# In[12]:
+
+
+a1
+
+
+# In[ ]:
+
+
+
+
