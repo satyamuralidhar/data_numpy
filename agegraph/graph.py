450baa3cda5e44ceabbd4788468c5a48852c60e3
satya <38804803+satyamuralidhar@users.noreply.github.com>
2019-09-06 15:07:12 +0530

    Create graph.py

---
+++ agegraph/graph.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+
+data = {
+    'Name':['satya' , 'Murali' , 'vijay' ,'pavan' ,'mohan' , 'durga', 'sam' , 'ram' ,'kalyan' ,'mani' , 'satish' , 'teja' , 'surya' , 'chandra'] , 'Age':[21 , 24 ,22 , 34 ,35, 24, 27, 31, 24, 23, 25 , 29 , 31, 29] ,'Department':['IT','IT','MECH','IT','PETROLIUM','ITI','CIVIL','MERINE','AERONAUTICS','HR','CIVIL','DOCTOR','AGRICULTURE','MECH']
+}
+
+df = pd.DataFrame(data)
+
+scatter plot:
+
+import matplotlib.pyplot as plt
+sns.FacetGrid(data = df , hue = 'Name' , size = 8).map(plt.scatter , 'Age' ,'Department').add_legend()
+plt.show()
+
+
+
+bar plot:
+
+import matplotlib.pyplot as plt
+sns.FacetGrid(data = df , hue = 'Name' , size = 8).map(plt.bar , 'Age' ,'Department').add_legend()
+plt.show()
